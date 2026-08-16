e6bad82b3b0b6dd4f46c221243b77db28ed10d22
siddharth singh <siddharthsingh20241@gmail.com>
2026-08-16 22:25:22 +0530

    leetcode: update Two Sum — Optimal (java)

--- LeetCode/Easy/two-sum/optimal.java
+++ LeetCode/Easy/two-sum/optimal.java
@@ -1,9 +1,9 @@
 class Solution {
-    public int[] twoSum(int[] arr, int target) {
+    public int[] twoSum(int[] a, int target) {
         int t,k;
-        for( int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]+arr[j]==target){
+        for( int i=0;i<a.length;i++){
+            for(int j=i+1;j<a.length;j++){
+                if(a[i]+a[j]==target){
                      return new int[]{i, j};
                 }
             }
