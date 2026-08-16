e702a7955a028490501d9ced3b1a888c70b14162
siddharth singh <siddharthsingh20241@gmail.com>
2026-08-17 03:41:13 +0530

    leetcode: update Two Sum — Optimal (cpp)

--- LeetCode/Easy/two-sum/optimal.cpp
+++ LeetCode/Easy/two-sum/optimal.cpp
@@ -1,18 +1,16 @@
-    vector<int> twoSum(vector<int>& arr, int x) {
-
-        vector<int> v(2);
-        int n = arr.size();
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (arr[i] + arr[j] == x) {
-                    v[0] = i;
-                    v[1] = j;
-                    return v;
-                }
-            }
-        }
-        return v;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
     }
 };
-public:
-class Solution {
