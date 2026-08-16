be4f8d96c53823cb21ef3c9a114102cda0faf970
siddharth singh <siddharthsingh20241@gmail.com>
2026-08-16 23:14:23 +0530

    codechef: add FLOW001 — Optimal (cpp)

---
+++ CodeChef/Difficulty:242/flow001/optimal.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        // your code goes here
+        cout<<a+b<<endl;
+        
+    }
+    //cout<<endl;
+    
+    return 0;
+}
+
+friend keyword
+lower_bound snippet
+upper_bound snippet
